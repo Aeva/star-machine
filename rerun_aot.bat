4e571e3a0081eaf37e6ac445e3653b067744b3b7
Aeva Palecek <aeva.ntsc@gmail.com>
2024-09-07 20:10:27 -0500

    moving more stuff into msbuild

---
+++ rerun_aot.bat
@@ -0,0 +1,2 @@
+
+.\bin\Release\net8.0\win-x64\publish\StarMachine %*
--- run_aot.bat
+++ run_aot.bat
@@ -1,7 +1,4 @@
 
 call build_shaders.bat
 dotnet publish -r win-x64 -c Release
-copy third_party\sdl3\Debug\SDL3.dll bin\Release\net8.0\win-x64\publish\
-copy third_party\plutovg\plutovg-0.dll bin\Release\net8.0\win-x64\publish\
-copy third_party\plutosvg\plutosvg-0.dll bin\Release\net8.0\win-x64\publish\
-.\bin\Release\net8.0\win-x64\publish\StarMachine
+.\bin\Release\net8.0\win-x64\publish\StarMachine %*
