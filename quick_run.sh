8a0d8d65e177e197e72c7bf27329bf82423b43d4
Aeva <aeva.ntsc@gmail.com>
2024-09-07 21:04:25 -0500

    dotnet run and publish now do the right things wrt linux .so files

--- quick_run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-dotnet build
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:bin/Release/net8.0/linux-x64/publish/ dotnet run
--- run_aot.sh
+++ run_aot.sh
@@ -2,7 +2,4 @@
 set -e
 sh build_shaders.sh
 dotnet publish -r linux-x64 -c Release
-# cp ../sdl3gpu/SDL/build/libSDL3.so bin/Release/net8.0/linux-x64/publish/
-# cp ../plutovg/build/libplutovg.so bin/Release/net8.0/linux-x64/publish/plutovg-0.so
-# cp ../plutosvg/build/libplutosvg.so bin/Release/net8.0/linux-x64/publish/plutosvg-0.so
 ./bin/Release/net8.0/linux-x64/publish/StarMachine $@
